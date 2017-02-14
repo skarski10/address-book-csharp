35213d12c53f77af10e072f5996cc5391421a327
Kory Skarbek <koryskarbek@yahoo.com>
2017-02-13 16:23:35 -0800

    fix Post for contact

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -24,13 +24,13 @@ namespace AddressBook
       };
 
       Get["/contact/{id}"] = parameters => {
-        Dictionary<string, object> model = new Dictionary<string, object>();
+//         Dictionary<string, object> model = new Dictionary<string, object>();
         Contact selectedContact = Contact.Find(parameters.id);
-        List<Address> contactAddresses = selectedContact.GetAddress();
-        List<Number> contactNumber = selectedContact.GetNumber();
-        model.Add("contact", selectedContact);
-        model.Add("address", contactAddresses);
-        model.Add("number", contactNumber);
+//         List<Address> contactAddresses = selectedContact.GetAddress();
+//         List<Number> contactNumber = selectedContact.GetNumber();
+//         model.Add("contact", selectedContact);
+//         model.Add("address", contactAddresses);
+//         model.Add("number", contactNumber);
         return View["contact.cshtml", model];
       };
 
@@ -67,7 +67,7 @@ namespace AddressBook
 //         List<Number> allNumbers = Number.GetAll();
 //         return View["contact_number.cshtml", allNumbers];
 //       };
-      Post["/contact/number"] = _ => {
+      Post["/contact"] = _ => {
         Contact selectedContact = Contact.Find(Request.Form["contact-id"]);
         List<Address> contactAddress = selectedContact.GetAddress();
         List<Number> contactNumber = selectedContact.GetNumber();
@@ -77,7 +77,7 @@ namespace AddressBook
         Number newNumber = new Number(numberEntered);
         contactAddress.Add(newAddress);
         contactNumber.Add(newNumber);
-        return View["contact_number.cshtml", selectedContact];
+        return View["contact.cshtml", selectedContact];
       };
     }
   }
